87bde8d4e5f705fc1981f2f5df5287175ab16206
kujjuarun <kujjuarun@yahoo.com>
2021-11-17 16:58:11 -0500

    jupyter project added

---
+++ jupyter/variable.tf
@@ -0,0 +1,19 @@
+variable "availability_zone" {
+  default = "us-east-1a"
+}
+
+variable "contact" {
+  default = "arun.lourduraj@gmail.com"
+}
+
+variable "environment" {
+  default = "TEST"
+}
+
+variable "instance_type" {
+  default = "t2.micro"
+}
+
+variable "service" {
+  default = "jupyter"
+}
