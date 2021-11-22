c89625858f23819606527df40053121b860b4be7
ramcloud2017 <90028088+ramcloud2017@users.noreply.github.com>
2021-11-22 21:57:28 +0530

    Update main.tf

--- ram-project2/main.tf
+++ ram-project2/main.tf
@@ -1,7 +1,7 @@
 provider "aws" {
   region = "ap-south-1"
-  access_key = "AKIA4LRI4O6HSNGRQ7Y7"
-  secret_key = "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"
+  access_key = " "
+  secret_key = ""
 }
 
 resource "aws_s3_bucket" "tf-marks-bucket" {
