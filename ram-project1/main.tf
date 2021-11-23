bbbc53ff4be1d1fccff85a6b1e7211246a397f00
ramcloud2017 <90028088+ramcloud2017@users.noreply.github.com>
2021-11-23 16:07:24 +0530

    Update main.tf

--- ram-project1/main.tf
+++ ram-project1/main.tf
@@ -1,7 +1,7 @@
 provider "aws" {
   region     = "ap-south-1"
-  access_key = ""
-  secret_key = ""
+  ##access_key = ""
+  ##secret_key = ""
 }
 resource "aws_instance" "my-server" {
   ami           = "ami-04bde106886a53080"
